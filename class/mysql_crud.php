aaead625d4fb09e295462c569a08e2ee9fbc2b5a
Karim Mohamed <37942217+KarimMohamed2005@users.noreply.github.com>
2020-06-16 15:55:07 +0200

    Update mysql_crud.php

--- class/mysql_crud.php
+++ class/mysql_crud.php
@@ -12,9 +12,9 @@ class Database{
 	 * Database class
 	 */
 	private $db_host = "localhost";  // Change as required
-	private $db_user = "user";  // Change as required
-	private $db_pass = "password";  // Change as required
-	private $db_name = "database";	// Change as required
+	private $db_user = "dplyr";  // Change as required
+	private $db_pass = "dplyr";  // Change as required
+	private $db_name = "dplyr";	// Change as required
 	
 	/*
 	 * Extra variables that are required by other function such as boolean con variable
